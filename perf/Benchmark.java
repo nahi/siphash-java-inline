35f86d41c6bf1be50707e30be41194ad656b5f14
Hiroshi Nakamura <nahi@ruby-lang.org>
2014-06-08 22:17:14 +0900

    Some uncommitted updates to perf check programs

--- perf/Benchmark.java
+++ perf/Benchmark.java
@@ -61,6 +61,16 @@ public class Benchmark {
         return System.nanoTime() - start;
     }
     
+    private static long sipHashInline4(byte[] msg) throws Exception {
+	long k0 = SipHashInlineTry.packLong(SPEC_KEY, 0);
+	long k1 = SipHashInlineTry.packLong(SPEC_KEY, 8);
+        long start = System.nanoTime();
+	for (int i = 0; i < TIMES; ++i) {
+	    SipHashInlineTry.digest4(k0, k1, msg);
+	}
+        return System.nanoTime() - start;
+    }
+    
     private static long murmurHash32(byte[] msg) throws Exception {
 	// downcast to 32bit
 	int seed = (int)UnsignedInt64.binToIntOffset(SPEC_KEY, 0); 
@@ -70,47 +80,72 @@ public class Benchmark {
 	}
         return System.nanoTime() - start;
     }
+    
+    private static long perlHash(byte[] msg) throws Exception {
+	// downcast to 32bit
+	int seed = (int)UnsignedInt64.binToIntOffset(SPEC_KEY, 0); 
+        long start = System.nanoTime();
+	for (int i = 0; i < TIMES; ++i) {
+	    PerlHash.hash(seed, msg, 0, msg.length);
+	}
+        return System.nanoTime() - start;
+    }
 
     public static void main(String[] args) throws Exception {
         long start = System.nanoTime();
-	for (int i = 0; i < 64; ++i) {
-    	    byte[] msg = createMsg(i);
+        while (true) {
+	    for (int i = 1; i <= 1024; i *= 2) {
+		byte[] msg = createMsg(i);
 
-	    System.out.print("" + i + ",SipHash,");
-	    for (int j = 0; j < 10; ++j) {
-    		System.out.print(sipHash(msg) + ",");
-	    }
-	    System.out.println();
+		System.out.print("" + i + ",SipHash,");
+		for (int j = 0; j < 10; ++j) {
+		    System.out.print(sipHash(msg) + ",");
+		}
+		System.out.println();
 
-	    System.out.print("" + i + ",SipHashBytes,");
-	    for (int j = 0; j < 10; ++j) {
-    		System.out.print(sipHashBytes(msg) + ",");
-	    }
-	    System.out.println();
+		System.out.print("" + i + ",SipHashBytes,");
+		for (int j = 0; j < 10; ++j) {
+		    System.out.print(sipHashBytes(msg) + ",");
+		}
+		System.out.println();
 
-	    System.out.print("" + i + ",SipHashInline,");
-	    for (int j = 0; j < 10; ++j) {
-    		System.out.print(sipHashInline(msg) + ",");
-	    }
-	    System.out.println();
+		System.out.print("" + i + ",SipHashInline,");
+		for (int j = 0; j < 10; ++j) {
+		    System.out.print(sipHashInline(msg) + ",");
+		}
+		System.out.println();
 
-	    System.out.print("" + i + ",SipHashInline2,");
-	    for (int j = 0; j < 10; ++j) {
-    		System.out.print(sipHashInline2(msg) + ",");
-	    }
-	    System.out.println();
+		System.out.print("" + i + ",SipHashInline2,");
+		for (int j = 0; j < 10; ++j) {
+		    System.out.print(sipHashInline2(msg) + ",");
+		}
+		System.out.println();
 
-	    System.out.print("" + i + ",SipHashInline3,");
-	    for (int j = 0; j < 10; ++j) {
-    		System.out.print(sipHashInline3(msg) + ",");
-	    }
-	    System.out.println();
+		System.out.print("" + i + ",SipHashInline3,");
+		for (int j = 0; j < 10; ++j) {
+		    System.out.print(sipHashInline3(msg) + ",");
+		}
+		System.out.println();
 
-	    System.out.print("" + i + ",Murmur2,");
-	    for (int j = 0; j < 10; ++j) {
-    		System.out.print(murmurHash32(msg) + ",");
+		System.out.print("" + i + ",SipHashInline4,");
+		for (int j = 0; j < 10; ++j) {
+		    System.out.print(sipHashInline4(msg) + ",");
+		}
+		System.out.println();
+
+		System.out.print("" + i + ",Murmur2,");
+		for (int j = 0; j < 10; ++j) {
+		    System.out.print(murmurHash32(msg) + ",");
+		}
+		System.out.println();
+
+		System.out.print("" + i + ",PerlHash,");
+		for (int j = 0; j < 10; ++j) {
+		    System.out.print(perlHash(msg) + ",");
+		}
+		System.out.println();
+
 	    }
-	    System.out.println();
         }
     }
 }
--- perf/SipHashInlineTry.java
+++ perf/SipHashInlineTry.java
@@ -1,5 +1,26 @@
 public class SipHashInlineTry {
 
+    private static final sun.misc.Unsafe UNSAFE;
+    static {
+      sun.misc.Unsafe unsafe = null;
+      try {
+        java.lang.reflect.Field field = sun.misc.Unsafe.class.getDeclaredField("theUnsafe");
+        field.setAccessible(true);
+        unsafe = (sun.misc.Unsafe)field.get(null);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      UNSAFE = unsafe;
+    }
+    private static final long base = UNSAFE.arrayBaseOffset(new byte[0].getClass());
+
+    // Assumes data[offset + 7] exists
+    public static long packLong(byte[] data, int offset) {
+        // read 8 bytes as BE long and reverse to LE
+        long m = UNSAFE.getLong(data, base + offset);
+        return Long.reverseBytes(m);
+    }
+
     // Assumes data[offset + 7] exists
     public static long pack8(byte[] data, int offset) {
         return
@@ -11,6 +32,162 @@ public class SipHashInlineTry {
             (long) data[offset + 5] << 40 |
             (long) data[offset + 6] << 48 |
             (long) data[offset + 7] << 56 ;
+    }
+
+    // SipHash with hand inlining and Unsafe array access
+    public static long digest4(long k0, long k1, byte[] data) {
+        long v0 = 0x736f6d6570736575L ^ k0;
+        long v1 = 0x646f72616e646f6dL ^ k1;
+        long v2 = 0x6c7967656e657261L ^ k0;
+        long v3 = 0x7465646279746573L ^ k1;
+        long m;
+        int dataLength = data.length;
+        int last = dataLength / 8 * 8;
+        int i = 0;
+
+        // processing 8 bytes blocks in data
+        while (i < last) {
+            // read 8 bytes as BE long and reverse to LE
+            m = UNSAFE.getLong(data, base + i);
+            m = Long.reverseBytes(m);
+            i += 8;
+
+            // MSGROUND {
+                v3 ^= m;
+
+                /* SIPROUND wih hand reordering
+                 *
+                 * SIPROUND in siphash24.c:
+                 *   A: v0 += v1;
+                 *   B: v1=ROTL(v1,13);
+                 *   C: v1 ^= v0;
+                 *   D: v0=ROTL(v0,32);
+                 *   E: v2 += v3;
+                 *   F: v3=ROTL(v3,16);
+                 *   G: v3 ^= v2;
+                 *   H: v0 += v3;
+                 *   I: v3=ROTL(v3,21);
+                 *   J: v3 ^= v0;
+                 *   K: v2 += v1;
+                 *   L: v1=ROTL(v1,17);
+                 *   M: v1 ^= v2;
+                 *   N: v2=ROTL(v2,32);
+                 *
+                 * Each dependency:
+                 *   B -> A
+                 *   C -> A, B
+                 *   D -> C
+                 *   F -> E
+                 *   G -> E, F
+                 *   H -> D, G
+                 *   I -> H
+                 *   J -> H, I
+                 *   K -> C, G
+                 *   L -> K
+                 *   M -> K, L
+                 *   N -> M
+                 *
+                 * Dependency graph:
+                 *   D -> C -> B -> A
+                 *        G -> F -> E
+                 *   J -> I -> H -> D, G
+                 *   N -> M -> L -> K -> C, G
+                 *
+                 * Resulting parallel friendly execution order:
+                 *   -> ABCDHIJ
+                 *   -> EFGKLMN
+                 */
+
+                // SIPROUND {
+                    v0 += v1;                    v2 += v3;
+                    v1 = (v1 << 13) | v1 >>> 51; v3 = (v3 << 16) | v3 >>> 48;
+                    v1 ^= v0;                    v3 ^= v2;
+                    v0 = (v0 << 32) | v0 >>> 32; v2 += v1;
+                    v0 += v3;                    v1 = (v1 << 17) | v1 >>> 47;
+                    v3 = (v3 << 21) | v3 >>> 43; v1 ^= v2;
+                    v3 ^= v0;                    v2 = (v2 << 32) | v2 >>> 32;
+                // }
+                // SIPROUND {
+                    v0 += v1;                    v2 += v3;
+                    v1 = (v1 << 13) | v1 >>> 51; v3 = (v3 << 16) | v3 >>> 48;
+                    v1 ^= v0;                    v3 ^= v2;
+                    v0 = (v0 << 32) | v0 >>> 32; v2 += v1;
+                    v0 += v3;                    v1 = (v1 << 17) | v1 >>> 47;
+                    v3 = (v3 << 21) | v3 >>> 43; v1 ^= v2;
+                    v3 ^= v0;                    v2 = (v2 << 32) | v2 >>> 32;
+                // }
+                v0 ^= m;
+            // }
+        }
+
+        // packing the last block to long, as LE 0-7 bytes + the length in the top byte
+        m = 0;
+        for (i = dataLength - 1; i >= last; --i) {
+            m <<= 8; m |= (long) data[i];
+        }
+        m |= (long) dataLength << 56;
+        // MSGROUND {
+            v3 ^= m;
+            // SIPROUND {
+                v0 += v1;                    v2 += v3;
+                v1 = (v1 << 13) | v1 >>> 51; v3 = (v3 << 16) | v3 >>> 48;
+                v1 ^= v0;                    v3 ^= v2;
+                v0 = (v0 << 32) | v0 >>> 32; v2 += v1;
+                v0 += v3;                    v1 = (v1 << 17) | v1 >>> 47;
+                v3 = (v3 << 21) | v3 >>> 43; v1 ^= v2;
+                v3 ^= v0;                    v2 = (v2 << 32) | v2 >>> 32;
+            // }<D-d>
+            // SIPROUND {
+                v0 += v1;                    v2 += v3;
+                v1 = (v1 << 13) | v1 >>> 51; v3 = (v3 << 16) | v3 >>> 48;
+                v1 ^= v0;                    v3 ^= v2;
+                v0 = (v0 << 32) | v0 >>> 32; v2 += v1;
+                v0 += v3;                    v1 = (v1 << 17) | v1 >>> 47;
+                v3 = (v3 << 21) | v3 >>> 43; v1 ^= v2;
+                v3 ^= v0;                    v2 = (v2 << 32) | v2 >>> 32;
+            // }
+            v0 ^= m;
+        // }
+
+        // finishing...
+        v2 ^= 0xff;
+        // SIPROUND {
+            v0 += v1;                    v2 += v3;
+            v1 = (v1 << 13) | v1 >>> 51; v3 = (v3 << 16) | v3 >>> 48;
+            v1 ^= v0;                    v3 ^= v2;
+            v0 = (v0 << 32) | v0 >>> 32; v2 += v1;
+            v0 += v3;                    v1 = (v1 << 17) | v1 >>> 47;
+            v3 = (v3 << 21) | v3 >>> 43; v1 ^= v2;
+            v3 ^= v0;                    v2 = (v2 << 32) | v2 >>> 32;
+        // }
+        // SIPROUND {
+            v0 += v1;                    v2 += v3;
+            v1 = (v1 << 13) | v1 >>> 51; v3 = (v3 << 16) | v3 >>> 48;
+            v1 ^= v0;                    v3 ^= v2;
+            v0 = (v0 << 32) | v0 >>> 32; v2 += v1;
+            v0 += v3;                    v1 = (v1 << 17) | v1 >>> 47;
+            v3 = (v3 << 21) | v3 >>> 43; v1 ^= v2;
+            v3 ^= v0;                    v2 = (v2 << 32) | v2 >>> 32;
+        // }
+        // SIPROUND {
+            v0 += v1;                    v2 += v3;
+            v1 = (v1 << 13) | v1 >>> 51; v3 = (v3 << 16) | v3 >>> 48;
+            v1 ^= v0;                    v3 ^= v2;
+            v0 = (v0 << 32) | v0 >>> 32; v2 += v1;
+            v0 += v3;                    v1 = (v1 << 17) | v1 >>> 47;
+            v3 = (v3 << 21) | v3 >>> 43; v1 ^= v2;
+            v3 ^= v0;                    v2 = (v2 << 32) | v2 >>> 32;
+        // }
+        // SIPROUND {
+            v0 += v1;                    v2 += v3;
+            v1 = (v1 << 13) | v1 >>> 51; v3 = (v3 << 16) | v3 >>> 48;
+            v1 ^= v0;                    v3 ^= v2;
+            v0 = (v0 << 32) | v0 >>> 32; v2 += v1;
+            v0 += v3;                    v1 = (v1 << 17) | v1 >>> 47;
+            v3 = (v3 << 21) | v3 >>> 43; v1 ^= v2;
+            v3 ^= v0;                    v2 = (v2 << 32) | v2 >>> 32;
+        // }
+        return v0 ^ v1 ^ v2 ^ v3;
     }
 
     // SipHash implementation with hand inlining the SIPROUND of siphash24.c
